24d9d01879570178b8fd53f3049fdc7616c63521
Casperliuliuliu <casperliu1118@gmail.com>
2024-02-17 16:38:23 +0800

    reimplementing DatePlannerAp

--- Date Planner.swiftpm/App/DatePlannerApp.swift
+++ Date Planner.swiftpm/App/DatePlannerApp.swift
@@ -1,22 +1,16 @@
-/*
-See the License.txt file for this sample’s licensing information.
-*/
-
 import SwiftUI
 
 @main
-struct DatePlannerApp: App {
+struct DatePlannerApp: App{
     @StateObject private var eventData = EventData()
-
-    var body: some Scene {
-        WindowGroup {
-            NavigationView {
+    var body:some Scene{
+        WindowGroup{
+            NavigationView{
                 EventList()
                 Text("Select an Event")
                     .foregroundStyle(.secondary)
             }
             .environmentObject(eventData)
-
         }
     }
 }
